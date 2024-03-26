a130f301ee627b3c6d31a6a06f3f05afd742731f
FHangH <752972182@qq.com>
2024-03-26 09:17:41 +0800

    Change: GitPush.bat

---
+++ GitPush_AddCommit.bat
@@ -0,0 +1,48 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: 设置控制台编码为UTF-8
+chcp 65001
+
+:: 执行git add .
+echo 正在添加所有更改...
+git add .
+echo 添加完成!
+
+:: 等待用户输入内容
+echo 请输入提交消息: 
+set /p message=
+
+:: 获取所有分支
+echo 正在获取所有分支...
+for /f "tokens=*" %%a in ('git branch') do (
+    set "branches=!branches! %%a"
+)
+
+:: 显示所有分支
+echo 可用分支:
+echo %branches%
+
+:: 等待用户输入分支名
+echo 请输入要提交的分支名 (按回车使用默认分支 master):
+set /p branch=
+
+:: 如果用户没有输入分支名，则使用默认分支 master
+if "%branch%" == "" (
+    set branch=master
+)
+
+:: 执行git commit -m "用户输入的内容"
+echo 正在提交更改...
+git commit -m "%message%"
+
+:: 执行git push
+echo 正在推送更改到%branch%分支...
+git push origin %branch%
+
+:: 等待用户按回车键
+echo 提交完成--按回车键退出...
+pause
+
+:: 结束
+endlocal
