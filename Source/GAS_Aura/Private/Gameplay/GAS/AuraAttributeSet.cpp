4962ef8e6030a33f521a2d499df002e578d42a57
FHangH <752972182@qq.com>
2024-03-30 16:59:46 +0800

    Change: AuraAttributeSet override PreAttributeChange()

--- Source/GAS_Aura/Private/Gameplay/GAS/AuraAttributeSet.cpp
+++ Source/GAS_Aura/Private/Gameplay/GAS/AuraAttributeSet.cpp
@@ -18,6 +18,20 @@ void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
 }
 
+void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+	}
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+	}
+}
+
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue) const
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldValue);
--- Source/GAS_Aura/Public/Gameplay/GAS/AuraAttributeSet.h
+++ Source/GAS_Aura/Public/Gameplay/GAS/AuraAttributeSet.h
@@ -68,6 +68,7 @@ public:
 	/* Function */
 	UAuraAttributeSet();
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
 	
 	UFUNCTION()
 	void OnRep_Health(const FGameplayAttributeData& OldValue) const;
