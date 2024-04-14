b2cfafe363a3716d38b2eacada3b63adfddc6677
FHangH <752972182@qq.com>
2024-04-14 12:16:43 +0800

    Change: Add execute tip Info GitPull_FromMaster.bat

--- GitPull_FromMaster.bat
+++ GitPull_FromMaster.bat
@@ -1,13 +1,26 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:ask
+:: Prompt the user to continue or not
+set /p continue="Do you want to continue (y/n)? "
+
+if /i "%continue%"=="y" (
+    goto pull
+) else if /i "%continue%"=="n" (
+    goto end
+) else (
+    echo Invalid input. Please enter 'y' or 'n'.
+    goto ask
+)
+
+:pull
 echo .Pulling from GitHub...
 git pull git@github.com:FHangH/GAS_Aura.git master
 
 :: 拉取完成
 echo .Pulling completed.
 
-pause
-
+:end
 :: 结束
 endlocal
