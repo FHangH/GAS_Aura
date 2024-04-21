9632846fe5a037686d463d3fa8c9918ea4469e26
FHangH <752972182@qq.com>
2024-04-21 10:35:25 +0800

    Change: EC_Damage Captruing Attributes, Armor, BlockChance; Implementing Target Block Chance

--- Source/GAS_Aura/Private/Gameplay/GAS/ExecCalc/EC_Damage.cpp
+++ Source/GAS_Aura/Private/Gameplay/GAS/ExecCalc/EC_Damage.cpp
@@ -3,9 +3,31 @@
 
 #include "Gameplay/GAS/ExecCalc/EC_Damage.h"
 #include "AbilitySystemComponent.h"
+#include "Gameplay/GAS/AuraAttributeSet.h"
+#include "Untils/AuraGameplayTags.h"
+
+struct AuraDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
+	
+	AuraDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false);
+	}
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	static AuraDamageStatics Statics;
+	return Statics;
+}
 
 UEC_Damage::UEC_Damage()
 {
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
 }
 
 void UEC_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -13,7 +35,33 @@ void UEC_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionPara
 {
 	const auto SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
 	const auto TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+	
 	const auto SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
 	const auto TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
 	const auto GESpec = ExecutionParams.GetOwningSpec();
+
+	const auto SourceTag = GESpec.CapturedSourceTags.GetAggregatedTags();
+	const auto TargetTag = GESpec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluateParams;
+	EvaluateParams.SourceTags = SourceTag;
+	EvaluateParams.TargetTags = TargetTag;
+
+	// Get Damage Set by Caller Magnitude
+	float Damage = GESpec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
+
+	// Capture BlockChance On Target and Determinate if it is Blocked
+	float TargetBlockChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(
+		DamageStatics().BlockChanceDef, EvaluateParams, TargetBlockChance);
+	TargetBlockChance = FMath::Max(0.f, TargetBlockChance);
+
+	// If Blocked => Halve the Damage
+	const auto bIsBlocked = FMath::FRandRange(1.f, 100.f) < TargetBlockChance;
+	Damage = bIsBlocked ? Damage / 2.f : Damage;
+	
+	const FGameplayModifierEvaluatedData Modifier(
+		UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
+	OutExecutionOutput.AddOutputModifier(Modifier);
 }
