d4d6acb0e56e6fde64fbef09d85621db7fa315a5
FHangH <752972182@qq.com>
2024-04-29 15:47:58 +0800

    Add: BTTask_Attack cpp File, BTT_Attack; Change: Set AI Behavior Melee Attacker Do Attack Task

---
+++ Source/GAS_Aura/Private/AI/BTTask_Attack.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright fangh.space
+
+
+#include "AI/BTTask_Attack.h"
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
---
+++ Source/GAS_Aura/Public/AI/BTTask_Attack.h
@@ -0,0 +1,17 @@
+﻿// Copyright fangh.space
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlueprintBase.h"
+#include "BTTask_Attack.generated.h"
+
+UCLASS()
+class GAS_AURA_API UBTTask_Attack : public UBTTask_BlueprintBase
+{
+	GENERATED_BODY()
+
+	/* Function */
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
