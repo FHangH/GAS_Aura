ce1c2e8b0a6283bc9667dbc2d1cf8865d4b530a9
FHangH <752972182@qq.com>
2024-05-29 16:10:49 +0800

    Add: AM_Shaman_Summon, GA_AuraSummon, GA_Summon; Change: Shaman Summon Spawn Locations

---
+++ Source/GAS_Aura/Private/Gameplay/GAS/GA/GA_AuraSummon.cpp
@@ -0,0 +1,47 @@
+﻿// Copyright fangh.space
+
+
+#include "Gameplay/GAS/GA/GA_AuraSummon.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Untils/AuraLog.h"
+
+TArray<FVector> UGA_AuraSummon::GetSpawnLocations()
+{
+	if (MinionClasses.IsEmpty() || MinionClasses[0] == nullptr)
+	{
+		UE_LOG(Aura, Warning, TEXT("MinionClasses Array is empty"));
+	}
+	if (!GetAvatarActorFromActorInfo()) return TArray<FVector>();
+
+	const auto Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const auto Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	const auto DeltaSpread = SpawnSpread / NumMinions;
+	TArray<FVector> SpawnLocations;
+
+	// Calculate LeftSpread To RightSpread Spawn Location
+	const auto LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);
+	for (int32 i = 0; i < NumMinions; ++i)
+	{
+		const auto Direction = LeftOfSpread.RotateAngleAxis(i * DeltaSpread, FVector::UpVector);
+		const auto ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+		SpawnLocations.Add(ChosenSpawnLocation);
+		
+		// Debug Really Spawn Location
+		DrawDebugSphere(GetWorld(), ChosenSpawnLocation, 18.f, 12, FColor::Cyan, false, 3.f);
+
+		// Debug Spawn Location Direction
+		UKismetSystemLibrary::DrawDebugArrow(
+			GetAvatarActorFromActorInfo(),
+			Location,
+			Location + Direction * MaxSpawnDistance,
+			4.f,
+			FLinearColor::Green,
+			3.f);
+
+		// Debug Spawn Location Range
+		DrawDebugSphere(GetWorld(), Location + Direction * MinSpawnDistance, 5.f, 12, FColor::Red, false, 3.f);
+		DrawDebugSphere(GetWorld(), Location + Direction * MaxSpawnDistance, 5.f, 12, FColor::Red, false, 3.f);
+	}
+	
+	return SpawnLocations;
+}
---
+++ Source/GAS_Aura/Public/Gameplay/GAS/GA/GA_AuraSummon.h
@@ -0,0 +1,35 @@
+﻿// Copyright fangh.space
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Gameplay/GAS/AuraGameplayAbility.h"
+#include "GA_AuraSummon.generated.h"
+
+UCLASS()
+class GAS_AURA_API UGA_AuraSummon : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+	/* Property */
+public:
+	UPROPERTY(EditDefaultsOnly, Category="Aura")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category="Aura")
+	int32 NumMinions {5};
+
+	UPROPERTY(EditDefaultsOnly, Category="Aura")
+	float MinSpawnDistance {50.f};
+	
+	UPROPERTY(EditDefaultsOnly, Category="Aura")
+	float MaxSpawnDistance {250.f};
+
+	UPROPERTY(EditDefaultsOnly, Category="Aura")
+	float SpawnSpread {90.f};
+
+	/* Function */
+public:
+	UFUNCTION(BlueprintPure, Category="Aura")
+	TArray<FVector> GetSpawnLocations();
+};
