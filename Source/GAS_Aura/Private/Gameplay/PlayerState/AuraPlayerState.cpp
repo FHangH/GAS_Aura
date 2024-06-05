839d7edc85d8b8c1e612f6070bdeb53d3c375864
FHangH <752972182@qq.com>
2024-06-05 11:31:01 +0800

    Change: AuraPlayerState.cpp, Adding XP to the PlayerState

--- Source/GAS_Aura/Private/Gameplay/PlayerState/AuraPlayerState.cpp
+++ Source/GAS_Aura/Private/Gameplay/PlayerState/AuraPlayerState.cpp
@@ -23,6 +23,7 @@ void AAuraPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Out
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
 	DOREPLIFETIME(AAuraPlayerState, Level);
+	DOREPLIFETIME(AAuraPlayerState, XP);
 }
 
 UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
@@ -30,7 +31,39 @@ UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
 	return ASComponent;
 }
 
+// Add
+void AAuraPlayerState::AddLevel(const int32 InLevel)
+{
+	Level += InLevel;
+	OnLevelChangedDelegate.Broadcast(Level);
+}
+
+void AAuraPlayerState::AddToXP(const int32 InXP)
+{
+	XP += InXP;
+	OnXPChangedDelegate.Broadcast(XP);
+}
+
+// Set
+void AAuraPlayerState::SetLevel(const int32 InLevel)
+{
+	Level = InLevel;
+	OnLevelChangedDelegate.Broadcast(Level);
+}
+
+void AAuraPlayerState::SetXP(const int32 InXP)
+{
+	XP = InXP;
+	OnXPChangedDelegate.Broadcast(XP);
+}
+
+// OnRepNotify
 void AAuraPlayerState::OnRep_Level(const int32 OldLevel) const
 {
-	
+	OnLevelChangedDelegate.Broadcast(Level);
+}
+
+void AAuraPlayerState::OnRep_XP(const int32 OldXP) const
+{
+	OnXPChangedDelegate.Broadcast(XP);
 }
