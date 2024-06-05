ba4a39fe1ed260ede5497dc82608ea67eadcb1e7
FHangH <752972182@qq.com>
2024-06-05 11:00:31 +0800

    Add: DataAsset_LevelUpInfo.cpp, DA_LevelUpInfo; Change: Async_WaitCooldownChange.cpp Delegate Displayname

---
+++ Source/GAS_Aura/Public/Gameplay/GAS/Data/DataAsset_LevelUpInfo.h
@@ -0,0 +1,37 @@
+﻿// Copyright fangh.space
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_LevelUpInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraLevelUpInfo
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 LevelUpRequirement { 0 };
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 AttributePointAward { 1 };
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 SpellPointAward { 1 };
+};
+
+UCLASS(BlueprintType)
+class GAS_AURA_API UDataAsset_LevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+	/* Property */
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Aura")
+	TArray<FAuraLevelUpInfo> LevelUpInformation;
+
+	/* Function */
+public:
+	int32 FindLevelForXP(const int32 XP) const;
+};
