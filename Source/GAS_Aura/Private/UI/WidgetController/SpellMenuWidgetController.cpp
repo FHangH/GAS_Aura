43d1875c3b9087284b7a430caa7dc4eebd86092e
FHangH <752972182@qq.com>
2024-06-21 15:48:00 +0800

    Change: WBP_SpellMenu Show Spell Points, Selecting and Deselecting WBP_SpellGlobe_Button

--- Source/GAS_Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/GAS_Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -4,21 +4,40 @@
 #include "UI/WidgetController/SpellMenuWidgetController.h"
 #include "Gameplay/GAS/AuraAbilitySystemComponent.h"
 #include "Gameplay/GAS/Data/DataAsset_AbilityInfo.h"
+#include "Gameplay/PlayerState/AuraPlayerState.h"
 
 void USpellMenuWidgetController::BindCallBackToDependencies()
 {
-	if (!GetAuraASC()) return;
-	GetAuraASC()->OnAbilityStatusChangedDelegate.AddLambda([this](const FGameplayTag& AbilityTag, const FGameplayTag& StatusTag)
+	if (GetAuraASC())
 	{
-		if (!DataAsset_AbilityInfo) return;
-
-		auto Info = DataAsset_AbilityInfo->FindAbilityInfoForTag(AbilityTag);
-		Info.StatusTag = StatusTag;
-		OnAbilityInfoDelegate.Broadcast(Info);
-	});
+		GetAuraASC()->OnAbilityStatusChangedDelegate.AddUObject(this, &ThisClass::OnAbilityStatusChanged);
+	}
+	if (GetAuraPS())
+	{
+		GetAuraPS()->OnSpellPointChangedDelegate.AddUObject(this, &ThisClass::OnSpellPointsChanged);
+	}
 }
 
 void USpellMenuWidgetController::BroadcastInitValues()
 {
 	BroadcastAbilityInfo();
+
+	if (GetAuraPS())
+	{
+		OnSpellPointsChangedDelegate.Broadcast(GetAuraPS()->GetSpellPoints());
+	}
+}
+
+void USpellMenuWidgetController::OnAbilityStatusChanged(const FGameplayTag& AbilityTag, const FGameplayTag& StatusTag) const
+{
+	if (!DataAsset_AbilityInfo) return;
+	
+	auto Info = DataAsset_AbilityInfo->FindAbilityInfoForTag(AbilityTag);
+	Info.StatusTag = StatusTag;
+	OnAbilityInfoDelegate.Broadcast(Info);
+}
+
+void USpellMenuWidgetController::OnSpellPointsChanged(const int32 SpellPoints) const
+{
+	OnSpellPointsChangedDelegate.Broadcast(SpellPoints);
 }
--- Source/GAS_Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
+++ Source/GAS_Aura/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -6,13 +6,24 @@
 #include "AuraWidgetController.h"
 #include "SpellMenuWidgetController.generated.h"
 
+struct FGameplayTag;
+
 UCLASS(BlueprintType, Blueprintable)
 class GAS_AURA_API USpellMenuWidgetController : public UAuraWidgetController
 {
 	GENERATED_BODY()
 
+	/* Property */
+protected:
+	UPROPERTY(BlueprintAssignable, Category="Aura|WidgetController|Messages")
+	FOnPlayerStatusChangedSignature OnSpellPointsChangedDelegate;
+
 	/* Function */
 public:
 	virtual void BindCallBackToDependencies() override;
 	virtual void BroadcastInitValues() override;
+
+protected:
+	void OnAbilityStatusChanged(const FGameplayTag& AbilityTag, const FGameplayTag& StatusTag) const;
+	void OnSpellPointsChanged(const int32 SpellPoints) const;
 };
