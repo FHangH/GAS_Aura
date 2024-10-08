4f8260a882d7488bcec7d64a107be453118c33ed
FHangH <752972182@qq.com>
2024-10-08 17:59:33 +0800

    Update: Ignore Enemies while Magic Circle Active;

--- Source/GAS_Aura/Private/Gameplay/PlayerController/AuraPlayerController.cpp
+++ Source/GAS_Aura/Private/Gameplay/PlayerController/AuraPlayerController.cpp
@@ -147,54 +147,6 @@ void AAuraPlayerController::CursorTrace()
 	LastActor = ThisActor;
 	ThisActor = Cast<IEnemyInterface>(CursorHitResult.GetActor());
 
-	/*
-	 * Line Trace from Cursor. There are many ways to do this
-	 * A. LastActor is null && ThisActor is null
-	 *		- Do nothing
-	 * B. LastActor is null && ThisActor is valid
-	 *		- Highlight ThisActor
-	 * C. LastActor is valid && ThisActor is null
-	 *		- Unhighlight LastActor
-	 * D. LastActor is valid && ThisActor is valid && ThisActor != LastActor
-	 *		- Unhighlight LastActor And Highlight ThisActor
-	 * E. LastActor is valid && ThisActor is valid && ThisActor == LastActor
-	 *		- Do nothing
-	 */
-#pragma region OldCode
-	/*if (LastActor == nullptr)
-	{
-		if (ThisActor!= nullptr)
-		{
-			// Case B
-			ThisActor->HighLightActor();
-		}
-		else
-		{
-			// Case A - Do nothing
-		}
-	}
-	else // LastActor is valid
-	{
-		if (ThisActor== nullptr)
-		{
-			// Case C
-			LastActor->UnHighLightActor();
-		}
-		else // Both Actor Valid
-		{
-			if (LastActor != ThisActor)
-			{
-				// Case D
-				LastActor->UnHighLightActor();
-				ThisActor->HighLightActor();
-			}
-			else
-			{
-				// Case E - Do nothing
-			}
-		}
-	}*/
-#pragma endregion
 	if (LastActor != ThisActor)
 	{
 		if (LastActor) LastActor->UnHighLightActor();
