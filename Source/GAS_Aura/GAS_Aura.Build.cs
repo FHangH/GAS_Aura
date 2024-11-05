07ecc09443cad8eb040bef641f401ceaf0a26efa
FHangH <752972182@qq.com>
2024-11-05 17:27:41 +0800

    Add: WBP_LoadSlot_Swicther, BP_LoadScreenGameMode, BP_LoadSreenHUD; Change: BP_LoadingManager, BP_LoadScreenGameMode; Add: LoadScreenHUD, MVVM_LoadScreen, LoadScreenWidget cpp files; View Model Class;

--- Source/GAS_Aura/GAS_Aura.Build.cs
+++ Source/GAS_Aura/GAS_Aura.Build.cs
@@ -11,7 +11,7 @@ public class GAS_Aura : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[]
 		{
 			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", 
-			"GameplayAbilities", "GameplayTags", "GameplayTasks", "NavigationSystem", "Niagara", "AIModule"
+			"GameplayAbilities", "GameplayTags", "GameplayTasks", "NavigationSystem", "Niagara", "AIModule", "ModelViewViewModel"
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
---
+++ Source/GAS_Aura/Public/UI/HUD/LoadScreenHUD.h
@@ -0,0 +1,13 @@
+﻿// Copyright fangh.space
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "LoadScreenHUD.generated.h"
+
+UCLASS()
+class GAS_AURA_API ALoadScreenHUD : public AHUD
+{
+	GENERATED_BODY()
+};
