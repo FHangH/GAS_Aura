2d5bb0ae7671c81274216a94ab01f8df26792042
FHangH <752972182@qq.com>
2024-11-06 14:42:07 +0800

    Add: WBP_LoadScreenWidget_Base, BP_MVVM_LoadScreen; Change: BP_LoadScreenHUD, WBP_LoadScreen; Change: LoadScreenHUD cpp file; Constructing a View Model;

--- Source/GAS_Aura/Private/UI/HUD/LoadScreenHUD.cpp
+++ Source/GAS_Aura/Private/UI/HUD/LoadScreenHUD.cpp
@@ -2,3 +2,32 @@
 
 
 #include "UI/HUD/LoadScreenHUD.h"
+#include "UI/MVVM/MVVM_LoadScreen.h"
+#include "UI/Widget/LoadScreenWidget.h"
+
+void ALoadScreenHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Create LoadScreen Widget
+	if (LoadScreenWidgetClass)
+	{
+		LoadScreenWidget = CreateWidget<ULoadScreenWidget>(GetWorld(), LoadScreenWidgetClass);
+		if (LoadScreenWidget) LoadScreenWidget->AddToViewport();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("LoadScreenWidgetClass is null"));
+	}
+
+	// Create LoadScreen MVVM
+	if (MVVM_LoadScreenClass)
+	{
+		MVVM_LoadScreen = NewObject<UMVVM_LoadScreen>(this, MVVM_LoadScreenClass);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("MVVM_LoadScreenClass is null"));
+	}
+}
+ 
--- Source/GAS_Aura/Public/UI/HUD/LoadScreenHUD.h
+++ Source/GAS_Aura/Public/UI/HUD/LoadScreenHUD.h
@@ -6,8 +6,30 @@
 #include "GameFramework/HUD.h"
 #include "LoadScreenHUD.generated.h"
 
+class UMVVM_LoadScreen;
+
 UCLASS()
 class GAS_AURA_API ALoadScreenHUD : public AHUD
 {
 	GENERATED_BODY()
+
+	/* Property */
+public:
+	// Widget
+	UPROPERTY(EditDefaultsOnly, Category="HUD|LoadScreen")
+	TSubclassOf<UUserWidget> LoadScreenWidgetClass;
+
+	UPROPERTY(BlueprintReadOnly, Category="HUD|LoadScreen")
+	TObjectPtr<UUserWidget> LoadScreenWidget;
+
+	// MVVM
+	UPROPERTY(EditDefaultsOnly, Category="HUD|MVVM")
+	TSubclassOf<UMVVM_LoadScreen> MVVM_LoadScreenClass;
+
+	UPROPERTY(BlueprintReadOnly, Category="HUD|MVVM")
+	TObjectPtr<UMVVM_LoadScreen> MVVM_LoadScreen;
+	
+	/* Function */
+protected:
+	virtual void BeginPlay() override;
 };
