2a1f20f7c82a2d7ff59cfc15d38b9cf8e25fd317
FHangH <752972182@qq.com>
2024-11-07 16:12:38 +0800

    Add: WBP_AreYouSure; Change: WBP_LoadScreen; Are You Sure Widget;

--- Source/GAS_Aura/Private/UI/HUD/LoadScreenHUD.cpp
+++ Source/GAS_Aura/Private/UI/HUD/LoadScreenHUD.cpp
@@ -2,7 +2,6 @@
 
 
 #include "UI/HUD/LoadScreenHUD.h"
-
 #include "Kismet/GameplayStatics.h"
 #include "UI/MVVM/MVVM_LoadScreen.h"
 #include "UI/Widget/LoadScreenWidget.h"
@@ -51,6 +50,7 @@ void ALoadScreenHUD::SetUserInputMode() const
 	const auto PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
 	FInputModeUIOnly InputMode_OnlyUI;
 	InputMode_OnlyUI.SetWidgetToFocus(LoadScreenWidget->TakeWidget());
+	InputMode_OnlyUI.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	PC->bShowMouseCursor = true;
 	PC->SetInputMode(InputMode_OnlyUI);
 }
